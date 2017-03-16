b8aad62e39ffa88a67088fa0a56dbdb5af12b5d6
Steffan <sachtmann121@gmail.com>
2017-03-16 21:33:47 +0000

    Contracts controller change

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -7,8 +7,8 @@ class ContactsController < ApplicationController
        @contact = Contact.new(contact_params)
        if @contact.save
            flash[:success] = "Message sent."
-           name = params{:contact][:name]
-           email = params{:contact][:email]
+           name = params[:contact][:name]
+           email = params[:contact][:email]
            body = params[:contact][:body]
            ContactMailer.contact_email(name, email, body).deliver
            redirect_to new_contact_path
